57e5df897f665f886d5ad7e99ddddaeae1b60ed9
doolun1312 <132755399+doolun1312@users.noreply.github.com>
2023-05-06 20:51:48 +0400

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,18 @@
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+// If the user scrolls, display the button
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
